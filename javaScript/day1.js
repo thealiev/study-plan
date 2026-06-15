d293e3661645930bde8c94f7fa894daa7a55ca0c
thealiev <tarqymuhammadal@gmail.com>
2026-06-16 02:27:20 +0500

    done

--- javaScript/day1.js
+++ javaScript/day1.js
@@ -1,97 +1,279 @@
-// const firstName = "Salohiddin"
-// const age = 21
-// const isStudent = true
+// 1-masala
 
-// console.log(firstName);
-// console.log(age);
-// console.log(isStudent);
+// const data = {
+//     name: "salohiddin",
+//     age: 21,
+//     city: "Tashkent",
+//     isProgrammer: true
+// }
 
-// const name = "ali"
-// const age = 25
-// const isMarried  = false
-// const children = null
-// let salary;
+// console.log(data)
 
-// console.log(typeof name);
+// 2-masala
 
+// const name = "Ali";
+// const age = 20;
+// const isStudent = true;
+// const money = null;
+// let phone;
 
-// const a = 25
-// const b = 10
+// console.log(typeof name)
+// console.log(typeof age);
+// console.log(typeof isStudent);
+// console.log(typeof money);
+// console.log(typeof phone);
+
+// 3-masala
+
+// const a = 25;
+// const b = 10;
+
 // console.log(a + b);
 // console.log(a - b);
 // console.log(a * b);
 // console.log(a / b);
 // console.log(a % b);
 
-// // taqqoslash operatorlari
-// console.log(10 > 5);
-// console.log(10 < 5);
-// console.log(10 === 10);
-// console.log(10 !== 5);
-
-// // logical 
-// console.log(true && false);
-// console.log(true || false);
-// console.log(!true);
-
-// // if else 
-//     const age = 22
-// if(age >= 18){
-//     console.log("adult");
-    
-// }else {
-//     console.log("minor");
-    
-// }
-
-
-// ////////////////////////////////////////////
+// 4-masala
 
 // const number = -5
-// if (number > 0) {
-//     console.log("musbat sonni");
-    
-// }else if (number < 0 ) {
-//     console.log("maxfiy sonni");
-    
-// }else {
-//     console.log("0ga teng");
-    
+
+// if (number < 0) {
+//     console.log("manfiy son")
+
+// } else {
+//     console.log("musbat");
 // }
 
+// const number = 10
 
-// const number  = 12
+// if (number > 0) {
+//     console.log("musbat");
+
+// }else if (number < 0) {
+//     console.log("manfiy");
+
+// }else {
+//     console.log("nol");
+
+// }
+
+// const number = 12;
 
 // if (number % 2 === 0) {
 //     console.log("juft");
-    
+
 // }else {
 //     console.log("toq");
-    
+
 // }
 
+// const age = 25
+
+// if (age >= 18) {
+//     console.log("Kirish mukin");
+
+// }else {
+//     console.log("kirish mumkin emas");
+
+// }
 
 // const score = 87
 
 // if (score >= 90) {
 //     console.log("a");
-    
+
 // }else if (score >= 80) {
 //     console.log("b");
-    
+
 // }else if (score >= 70) {
 //     console.log("c");
-    
+
 // }else if (score >= 60) {
 //     console.log("d");
-    
+
 // }else {
-//     console.log("f")
+//     console.log("f");
+
+// }
+
+// for (let i = 1; i <= 50; i++) {
+//     console.log(i);
+
+// }
+
+// for (let i = 2; i <= 100; i += 2) {
+//         console.log(i);
+// }
+
+// for (let i = 1; i <=100; i += 2) {
+//     console.log(i);
+
+// }
+
+// let sum = 0
+
+// for (let i = 1; i <= 100; i ++) {
+
+//     sum += i
+
+// }
+
+// console.log(sum);
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i * i);
+// }
+
+// function sum(param1, param2) {
+//       return param1 + param2
+// }
+
+// console.log(sum(10,20));
+
+// function multiply(param1, param2) {
+//     return param1 * param2
+// }
+// console.log(sum(10,20));
+
+// function square(num) {
+//     return num * num
+// }
+
+// console.log(square(5));
+
+// function cube(num) {
+//     return num ** 3
+// }
+
+// console.log(cube(5));
+
+// function isEven(num) {
+//     return num % 2 === 0
+// }
+// console.log(isEven(8));
+
+// function checkAge(age) {
+//     return age >= 18
+// }
+
+// console.log(checkAge(22));
+
+// const data = {
+//     name: "Salohiddin",
+//     age: 21,
+//     job: "barista",
+//     city: "Tashkent",
+//     hobby: "Chess",
+//     experience: 2,
+//     phone: "999382920",
+//     email: "geolels82@gmail.com",
+//     language: "uzbek",
+//     goal: "freedom"
+// };
+
+// console.log(data.goal);
+
+// const user = {
+//    country: "uzbekistan"
+// }
+
+// user.city = "tashkent"
+
+// console.log(user);
+
+// const user = {
+//     age: 21
+// }
+
+// user.age = 23
+// console.log(user);
+
+// let sum = 0
+
+// const numbers = [10, 5, 4, 20, 30, 10]
+
+// for (let i = 0; i < numbers.length; i++) {
+
+//     sum += numbers[i]
+
+// }
+
+// console.log(sum);
+
+// let max = numbers[0];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+// }
+
+// console.log(max);
+
+// let min = numbers[0];
+
+// for (let i = 0; i < numbers.length; i++){
+//     if(numbers[i] < min){
+//         min = numbers[i]
+//     }
+// }
+
+// console.log(min);
+
+
+// const numbers = [10, 5, 7, 20, 50, 3, 1];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 !== 0) {
+//         console.log(numbers[i]);
+        
+//     }
+// }
+
+const users = [
+  {
+    id: 1,
+    name: "Ali",
+    age: 20
+  },
+  {
+    id: 2,
+    name: "Vali",
+    age: 25
+  },
+  {
+    id: 3,
+    name: "Sardor",
+    age: 30
+  }
+];
+
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i].age >= 25){
+//         console.log(users[i].age);
+        
+//     }
+// }
+
+// console.log(users);
+
+
+// for (let i = 0; i < users.length; i++) {
+//     console.log(users[i].name);
+    
+// }
+
+// function findUserById(id) {
+//    for (let i = 0; i < users.length; i++) {
+//     if(users[i].id === id) {
+//         return users[i]
+//     }
+//    }
 // }
 
 
-
-
-
-
+function createNewUser(id) {
+    
+}
 
