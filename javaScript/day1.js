cf24f5c268f6b6e34189a9c8c1fe452da9aff1bb
thealiev <tarqymuhammadal@gmail.com>
2026-06-15 11:56:10 +0500

    done

---
+++ javaScript/day1.js
@@ -0,0 +1,97 @@
+// const firstName = "Salohiddin"
+// const age = 21
+// const isStudent = true
+
+// console.log(firstName);
+// console.log(age);
+// console.log(isStudent);
+
+// const name = "ali"
+// const age = 25
+// const isMarried  = false
+// const children = null
+// let salary;
+
+// console.log(typeof name);
+
+
+// const a = 25
+// const b = 10
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// // taqqoslash operatorlari
+// console.log(10 > 5);
+// console.log(10 < 5);
+// console.log(10 === 10);
+// console.log(10 !== 5);
+
+// // logical 
+// console.log(true && false);
+// console.log(true || false);
+// console.log(!true);
+
+// // if else 
+//     const age = 22
+// if(age >= 18){
+//     console.log("adult");
+    
+// }else {
+//     console.log("minor");
+    
+// }
+
+
+// ////////////////////////////////////////////
+
+// const number = -5
+// if (number > 0) {
+//     console.log("musbat sonni");
+    
+// }else if (number < 0 ) {
+//     console.log("maxfiy sonni");
+    
+// }else {
+//     console.log("0ga teng");
+    
+// }
+
+
+// const number  = 12
+
+// if (number % 2 === 0) {
+//     console.log("juft");
+    
+// }else {
+//     console.log("toq");
+    
+// }
+
+
+// const score = 87
+
+// if (score >= 90) {
+//     console.log("a");
+    
+// }else if (score >= 80) {
+//     console.log("b");
+    
+// }else if (score >= 70) {
+//     console.log("c");
+    
+// }else if (score >= 60) {
+//     console.log("d");
+    
+// }else {
+//     console.log("f")
+// }
+
+
+
+
+
+
+
