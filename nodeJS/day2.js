9c55b51f2bdf6b0620c367ca7c4d75a6451f8b23
thealiev <tarqymuhammadal@gmail.com>
2026-06-18 00:16:24 +0500

    done

--- nodeJS/day2.js
+++ nodeJS/day2.js
@@ -1,11 +1,45 @@
-const express = requuire("express");
-
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello backend");
+const users = [
+  { id: 1, name: "Ali" },
+  { id: 2, name: "Vali" },
+];
+
+// app.get("/users/:id", (req, res) => {
+// const id = Number(req.params.id)  id olish
+// const user = users.find(u => u.id === id)   qidirish
+// res.send(user) qaytarish
+// });
+
+// ##CRUD
+
+// app.get("/users", (req, res) => {
+//   res.send(users)
+// })
+
+// app.get("/users/:id", (req, res) => {
+//   const id = Number(req.params.id)
+//   const user = users.find(u => u.id === id)
+//   res.send(user)
+// })
+
+// app.post("/users", (req, res) => {
+//   users.push(newUsers);
+//   res.send(users);
+// });
+
+app.del("/users:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((u) => u.id === id);
+
+  if (user) {
+    user.name = req.body.name;
+  }
+
+  res.send(user);
 });
 
 app.listen(3000, () => {
-  console.log("server running");
+  console.log("server runnning...");
 });
